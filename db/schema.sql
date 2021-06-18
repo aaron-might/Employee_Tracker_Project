82e13822394ee6c64a650aa1ed575855b6f5bc21
aaron-might <aaronmighty7@gmail.com>
2021-06-17 21:48:56 -0400

    insert values into departments, roles and employees table

--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,12 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 
-create TABLE department:(
+create TABLE department(
   id INT AUTO_INCREMENT PRIMARY KEY,
   department_name  VARCHAR(30) 
 );
 
-create TABLE role:(
+create TABLE role(
   id INT AUTO_INCREMENT PRIMARY KEY,
   title  VARCHAR(30),
   salary DECIMAL(10,2),
@@ -16,12 +16,12 @@ create TABLE role:(
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-create TABLE employee:(
+create TABLE employee(
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30)NOT NULL,
   last_name VARCHAR(30)NOT NULL,
   role_id INT,
   manager_id INT,
   FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES manager(id) 
+  FOREIGN KEY (manager_id) REFERENCES employee(id) 
 );
