3100f06d1ae227617fc706c5cb868d6c5429e2ea
aaron-might <aaronmighty7@gmail.com>
2021-06-17 23:03:24 -0400

    writing inquirer prompt

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,26 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 
-create TABLE department(
+create TABLE departments(
   id INT AUTO_INCREMENT PRIMARY KEY,
   department_name  VARCHAR(30) 
 );
 
-create TABLE role(
+create TABLE roles (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title  VARCHAR(30),
   salary DECIMAL(10,2),
   department_id  INT,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  FOREIGN KEY (id) REFERENCES departments(id)
 );
 
-create TABLE employee(
+create TABLE employees(
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30)NOT NULL,
   last_name VARCHAR(30)NOT NULL,
   role_id INT,
   manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id) 
+  FOREIGN KEY (id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id) 
 );
+SELECT * FROM roles;
--- db/seeds.sql
+++ db/seeds.sql
@@ -8,17 +8,17 @@ VALUES
 ('Engineering'),
 ('Marketing');
 
-INSERT INTO roles(title,salary,department_id)
+INSERT INTO roles (title,salary,department_id)
 VALUES 
 ('Regional Manager',102000,1),
-('Finance Rep',102000,2)
-('Quality control Rep',70000,3)
-('Warehouse Rep',45000,8)
+('Finance Rep',102000,2),
+('Quality control Rep',70000,3),
+('Warehouse Rep',45000,8),
 ('Sales Rep',68000,9),
 ('Engineering Rep',10000,6),
 ('Marketing Rep',70600,7);
 
-INSERT INTO employee(first_name,last_name,role_id)
+INSERT INTO employees(first_name,last_name,role_id)
 VALUES 
 ('Ryan','John',1),
 ('John','mann',2),
@@ -26,4 +26,8 @@ VALUES
 ('julia','Deloitte',9),
 ('Francine','Oreal',3),
 ('Alex','Johnson',6),
-('Jacqueline','batt',1),
+('Jacqueline','batt',1);
+
+UPDATE `employee_db`.`employee` 
+SET `manager_id` ='1'
+WHERE (`id`>'1');
