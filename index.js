b10dc0929c92f30ea5b5e741c74772bdc7b3ac66
aaron-might <aaronmighty7@gmail.com>
2021-06-18 09:11:33 -0400

    writing const addEmployee function and inquirer prompt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+
 // create the connection information for the sql database
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -16,8 +17,9 @@ const connection = mysql.createConnection({
   database: 'employee_db',
 });
 
+const logo = require('asciiart-logo');
 const chalk = require('chalk');
-const cTable = require('console.table');
+const Table = require('console.table');
 const startScreen = ['View all Employees', 'View all Employees by Department', 'View all Employees by Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'View all Roles', 'Add Role', 'Remove Role', 'View all Departments', 'Add Department', 'Remove Department', 'Exit']
 const allEmployeeQuery = `SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", r.title, d.department_name AS "Department", IFNULL(r.salary, 'No Data') AS "Salary", CONCAT(m.first_name," ",m.last_name) AS "Manager"
 FROM employees e
@@ -94,4 +96,118 @@ const showAll = () => {
         startApp();
     })
 
+}
+const showByDept = () => {
+    const deptQuery = 'SELECT * FROM departments';
+    connection.query(deptQuery, (err, results) => {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                name: 'deptChoice',
+                type: 'list',
+                choices: function () {
+                    let choiceArray = results.map(choice => choice.department_name)
+                    return choiceArray;
+                },
+                message: 'Select a Department to view:'
+            }
+        ]).then((answer) => {
+            let chosenDept;
+            for (let i = 0; i < results.length; i++) {
+                if (results[i].department_name === answer.deptChoice) {
+                    chosenDept = results[i];
+                }
+            }
+
+            const query = 'SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", r.title AS "Title", d.department_name AS "Department", r.salary AS "Salary" FROM employees e INNER JOIN roles r ON r.id = e.role_id INNER JOIN departments d ON d.id = r.department_id WHERE ?;';
+            connection.query(query, { department_name: chosenDept.department_name }, (err, res) => {
+                if (err) throw err;
+                console.log(' ');
+                console.table(chalk.yellow(`All Employees by Department: ${chosenDept.department_name}`), res)
+                startApp();
+            })
+        })
+    })
+}
+
+const showByManager = () => {
+    connection.query(mgrQuery, (err, results) => {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                name: 'mgr_choice',
+                type: 'list',
+                choices: function () {
+                    let choiceArray = results.map(choice => choice.full_name);
+                    return choiceArray;
+                },
+                message: 'Select a Manager:'
+            }
+        ]).then((answer) => {
+            const mgrQuery2 = `SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", IFNULL(r.title, "No Data") AS "Title", IFNULL(d.department_name, "No Data") AS "Department", IFNULL(r.salary, 'No Data') AS "Salary", CONCAT(m.first_name," ",m.last_name) AS "Manager"
+                FROM employees e
+                LEFT JOIN roles r 
+                ON r.id = e.role_id 
+                LEFT JOIN departments d 
+                ON d.id = r.department_id
+                LEFT JOIN employees m ON m.id = e.manager_id
+                WHERE CONCAT(m.first_name," ",m.last_name) = ?
+                ORDER BY e.id;`
+            connection.query(mgrQuery2, [answer.mgr_choice], (err, results) => {
+                if (err) throw err;
+                console.log(' ');
+                console.table(chalk.yellow('Employees by Manager'), results);
+                startApp();
+            })
+        })
+    })
+}
+
+const addEmployee = () => {
+    connection.query(roleQuery, (err, results) => {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                name: 'fName',
+                type: 'input',
+                message: addEmployeeQuestions[0]
+
+            },
+            {
+                name: 'lName',
+                type: 'input',
+                message: addEmployeeQuestions[1]
+            },
+            {
+                name: 'role',
+                type: 'list',
+                choices: function () {
+                    let choiceArray = results[0].map(choice => choice.title);
+                    return choiceArray;
+                },
+                message: addEmployeeQuestions[2]
+
+            },
+            {
+                name: 'manager',
+                type: 'list',
+                choices: function () {
+                    let choiceArray = results[1].map(choice => choice.full_name);
+                    return choiceArray;
+                },
+                message: addEmployeeQuestions[3]
+
+            }
+        ]).then((answer) => {
+            connection.query(
+                `INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES(?, ?, 
+                (SELECT id FROM roles WHERE title = ? ), 
+                (SELECT id FROM (SELECT id FROM employees WHERE CONCAT(first_name," ",last_name) = ? ) AS tmptable))`, [answer.fName, answer.lName, answer.role, answer.manager]
+            )
+            startApp();
+        })
+    })
 }
