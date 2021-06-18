286db5cbdffd6ac6c54736a2479cc7085a6aa0c0
aaron-might <aaronmighty7@gmail.com>
2021-06-17 21:05:08 -0400

    create db folder and move sql file in it, create employee db in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+
+create TABLE department:(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  department_name  VARCHAR(30) 
+);
+
+create TABLE role:(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title  VARCHAR(30),
+  salary DECIMAL(10,2),
+  department_id  INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+create TABLE employee:(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30)NOT NULL,
+  last_name VARCHAR(30)NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES manager(id) 
+);
